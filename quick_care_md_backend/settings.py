2dd7c7196be2e66ca252bb438084a22f484c444d
daroo22 <rodneysomto@gmail.com>
2024-09-23 15:48:53 -0400

    commit

--- quick_care_md_backend/settings.py
+++ quick_care_md_backend/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'rest_framework_simplejwt',
     'corsheaders',
     'main_app',  # Add your main application
 ]
